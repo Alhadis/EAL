df9b264917e787cfc874d3327fbe082f19e758cd
Alhadis <gardnerjohng@gmail.com>
2020-10-18 12:54:48 +1100

    Remove `nih()` from exports

--- index.mjs
+++ index.mjs
@@ -1,4 +1,7 @@
-export * from "./lib/hooks.mjs";
 export * from "./lib/flags.mjs";
 export * from "./lib/shims.mjs";
 export * from "./lib/fs.mjs";
+export {
+	getPolyfill,
+	setPolyfill,
+} from "./lib/hooks.mjs";
